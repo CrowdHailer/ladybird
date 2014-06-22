85f97ae51cac34915700d72a8951bc5184af3dec
Peter <peterhsaxton@gmail.com>
2014-06-22 17:58:12 +0100

    multi replacements

--- spec/ladybird_spec.js
+++ spec/ladybird_spec.js
@@ -3,4 +3,9 @@ describe('object interpolation', function(){
     var frame = ladybird('Hello, %(name)!');
     expect(frame({name: 'World'})).toEqual('Hello, World!');
   });
+
+  it('should be able to replace a multiple items', function(){
+    var frame = ladybird('The quick brown %(f) jumps over the lazy %(d).');
+    expect(frame({"f": "fox", "d": "dog"})).toEqual('The quick brown fox jumps over the lazy dog.');
+  });
 });
--- src/ladybird.js
+++ src/ladybird.js
@@ -1,5 +1,9 @@
 function ladybird (frame){
   return function(content){
-    return 'Hello, World!';
+    frame = frame.replace(/%\(([^)]+)\)/g, function(match, capture, offset, a){
+      console.log(match, capture, offset);
+      return content[capture];
+    });
+    return frame;
   };
 }
