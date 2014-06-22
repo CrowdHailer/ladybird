9f69425b7b1ed541b49e05dfef514ef3d877c2b4
Peter <peterhsaxton@gmail.com>
2014-06-22 18:04:20 +0100

    beginning and end of string tests

--- spec/ladybird_spec.js
+++ spec/ladybird_spec.js
@@ -8,4 +8,14 @@ describe('object interpolation', function(){
     var frame = ladybird('The quick brown %(f) jumps over the lazy %(d).');
     expect(frame({"f": "fox", "d": "dog"})).toEqual('The quick brown fox jumps over the lazy dog.');
   });
+
+  it('should replace multiple occurances at the front of a string', function(){
+    var frame = ladybird('%(go) %(go) gadget');
+    expect(frame({go: 'schnell'})).toEqual('schnell schnell gadget');
+  });
+
+  it('should work at the end of a string', function(){
+    var frame = ladybird('eats %(food)');
+    expect(frame({food: 'aphids'})).toEqual('eats aphids');
+  });
 });
