a4b1d719c719bdb420317abf253bcb3128d46467
Peter <peterhsaxton@gmail.com>
2014-07-11 08:43:59 +0100

    remove logging

--- src/ladybird.js
+++ src/ladybird.js
@@ -3,7 +3,7 @@ function ladybird (frame, defaults){
   return function(content){
     content = defaults(content);
     frame = frame.replace(/%\(([^)]+)\)/g, function(match, capture, offset, a){
-      console.log(match, capture, offset);
+      // console.log(match, capture, offset);
       return content[capture];
     });
     return frame;
